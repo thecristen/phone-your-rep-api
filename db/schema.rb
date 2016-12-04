dcaf7d0aff62dfeb6ff621c5df36d934408fca7a
thecristen <cristen@outlook.com>
2016-12-04 17:52:04 -0500

    add postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161204214341) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reps", force: :cascade do |t|
     t.text     "phone"
     t.text     "name"
