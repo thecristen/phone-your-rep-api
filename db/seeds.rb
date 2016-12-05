0d136f62a9ab2b489bbb656856efcec5606663c3
M. Simon Borg <msimonborg@gmail.com>
2016-12-05 16:46:30 -0500

    second iteration on seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,5 @@
 require 'csv'
 
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'us_senators_20161205.csv')) # reads file into local variable
-puts csv_text # run rails db:seed to print data to terminal to test contents of the variable
+csv = CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1') # parse the CSV for ruby, ignoring first line (headers)
+puts csv # run rails db:seed to print data to terminal to test contents of the variable
