fdc86f308c02d88f43aede6fde257fbe2598cacc
M. Simon Borg <msimonborg@gmail.com>
2016-12-05 18:30:57 -0500

    run zipcode migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205221222) do
+ActiveRecord::Schema.define(version: 20161205232543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20161205221222) do
     t.string   "photo"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
+  end
+
+  create_table "zipcodes", force: :cascade do |t|
+    t.integer  "zip"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
