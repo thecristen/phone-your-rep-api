57fc1b50e194b4ba9f15c9f5c6440d18dbbbafd2
M. Simon Borg <msimonborg@gmail.com>
2017-01-10 10:53:43 -0500

    import shapefiles geometries to districts and states

--- lib/seeds/20161221232814_import_districts_from_shape.rb
+++ lib/seeds/20161221232814_import_districts_from_shape.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
-# class ImportDistrictsFromShape < ActiveRecord::Migration[5.0]
-#   def up
-#     from_shp_sql = `shp2pgsql -c -g geom -W LATIN1 -s 4326 #{Rails.root.join('lib', 'shapefiles', 'us_congress_districts_122116', 'cb_2015_us_cd114_500k.shp')} districts_ref`
-#
-#     District.transaction do
-#       execute from_shp_sql
-#
-#       execute <<-SQL
-#           insert into districts(code, state_code, full_code, geom)
-#             select CD114FP, STATEFP, GEOID, geom from districts_ref
-#       SQL
-#
-#       d# rop_table :districts_ref
-#     end
-#   end
-#
-#   def down
-#     District.delete_all
-#   end
-# end
+class ImportDistrictsFromShape < ActiveRecord::Migration[5.0]
+ def up
+   from_shp_sql = `shp2pgsql -c -g geom -W LATIN1 -s 4326 #{Rails.root.join('lib', 'shapefiles', 'us_congress_districts_122116', 'cb_2015_us_cd114_500k.shp')} districts_ref`
+
+   District.transaction do
+     execute from_shp_sql
+
+     execute <<-SQL
+         insert into districts(code, state_code, full_code, geom)
+           select CD114FP, STATEFP, GEOID, geom from districts_ref
+     SQL
+
+     drop_table :districts_ref
+   end
+ end
+
+ def down
+   District.delete_all
+ end
+end
---
+++ lib/shapefiles/shapefiles.rb
@@ -0,0 +1,29 @@
+require_relative '../../config/environment.rb'
+
+def import_state_geoms
+  RGeo::Shapefile::Reader.open(Rails.root.join('lib', 'shapefiles', 'us_states_122116', 'cb_2015_us_state_500k.shp').to_s) do |file|
+    puts "File contains #{file.num_records} records."
+    file.each do |record|
+      puts "Record number #{record.index}:"
+      state = State.find_by(state_code: record.attributes['STATEFP'])
+      state.update(geom: record.geometry)
+      puts record.attributes
+    end
+  end
+end
+
+def import_district_geoms
+  RGeo::Shapefile::Reader.open(Rails.root.join('lib', 'shapefiles', 'us_congress_districts_122116', 'cb_2015_us_cd114_500k.shp').to_s) do |file|
+    puts "File contains #{file.num_records} records."
+    file.each do |record|
+      puts "Record number #{record.index}:"
+      district = District.find_by(full_code: record.attributes['GEOID'])
+      district.update(geom: record.geometry)
+      puts record.attributes
+    end
+  end
+end
+
+import_state_geoms
+
+import_district_geoms
