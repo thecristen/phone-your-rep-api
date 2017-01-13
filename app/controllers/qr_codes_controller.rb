dbc492ecfb74c6308189386571d5231488bd6b0f
M. Simon Borg <msimonborg@gmail.com>
2017-01-13 00:08:10 -0500

    qr_code is for v_card data, not link

--- app/controllers/qr_codes_controller.rb
+++ app/controllers/qr_codes_controller.rb
@@ -3,7 +3,8 @@ class QrCodesController < ActionController::Base
   def show
     if params[:id]
       @office = OfficeLocation.find(params[:id])
-      @qr = RQRCode::QRCode.new(@office.v_card_link, :size => 6, :level => :h)
+      @card = @office.make_vcard
+      @qr = RQRCode::QRCode.new(@card.to_s, :size => 21, :level => :h)
     end
   end
 end
