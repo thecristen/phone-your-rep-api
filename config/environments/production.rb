a968d771a418092311d8078d07dbc8ad9b712d79
M. Simon Borg <msimonborg@gmail.com>
2017-01-13 18:28:38 -0500

    rack-cache config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  config.action_dispatch.rack_cache = true
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
