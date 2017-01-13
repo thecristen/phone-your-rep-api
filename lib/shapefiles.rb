7e87fcc94ffe3a69a364233386cee873ee8823a4
M. Simon Borg <msimonborg@gmail.com>
2017-01-13 14:33:37 -0500

    clean up duplication

--- lib/shapefiles.rb
+++ lib/shapefiles.rb
@@ -1,33 +1,29 @@
 require_relative '../config/environment.rb'
 
-def import_state_geoms
+def import_geoms(dir:, shp_file:, model:, model_attr:, record_attr:)
   RGeo::Shapefile::Reader.open(Rails.root.join(
-    'lib', 'shapefiles', 'us_states_122116', 'cb_2015_us_state_500k.shp'
+    'lib', 'shapefiles', dir, shp_file
   ).to_s) do |file|
     puts "File contains #{file.num_records} records."
     file.each do |record|
       puts "Record number #{record.index}:"
-      state = State.find_by(state_code: record.attributes['STATEFP'])
-      state.update(geom: record.geometry)
+      instance = model.find_by(model_attr => record.attributes[record_attr])
+      instance.update(geom: record.geometry)
       puts record.attributes
     end
   end
 end
 
-def import_district_geoms
-  RGeo::Shapefile::Reader.open(Rails.root.join(
-    'lib', 'shapefiles', 'us_congress_districts_122116', 'cb_2015_us_cd114_500k.shp'
-  ).to_s) do |file|
-    puts "File contains #{file.num_records} records."
-    file.each do |record|
-      puts "Record number #{record.index}:"
-      district = District.find_by(full_code: record.attributes['GEOID'])
-      district.update(geom: record.geometry)
-      puts record.attributes
-    end
-  end
-end
+import_geoms(dir:         'us_states_122116',
+             shp_file:    'cb_2015_us_state_500k.shp',
+             model:       State,
+             model_attr:  :state_code,
+             record_attr: 'STATEFP')
 
-import_state_geoms
+import_geoms(dir:         'us_congress_districts_122116',
+             shp_file:    'cb_2015_us_cd114_500k.shp',
+             model:       District,
+             model_attr:  :full_code,
+             record_attr: 'GEOID')
 
-import_district_geoms
+#import_geoms(dir: dir, shp_file: shp_file, model: model, model_attr: model_attr, record_attr: record_attr)
