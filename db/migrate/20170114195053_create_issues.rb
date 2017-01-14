d59a8155a3ad6aa028bb1d5fa398f564bb8197b8
catkhuu <sunshine.dumbo@gmail.com>
2017-01-14 16:56:54 -0500

    Renamed column 'type' in Issue to 'issue_type'.

--- db/migrate/20170114195053_create_issues.rb
+++ db/migrate/20170114195053_create_issues.rb
@@ -1,8 +1,8 @@
 class CreateIssues < ActiveRecord::Migration[5.0]
   def change
     create_table :issues do |t|
-      t.string  :type, null: false
-      t.boolean :resolved?, default: false
+      t.string  :issue_type, null: false
+      t.boolean :resolved, default: false
       t.references  :office_location
       t.timestamps
     end
