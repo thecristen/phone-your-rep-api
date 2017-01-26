6dfa95c5867d8e934fd220ae3911e4b53c7b64d3
M. Simon Borg <msimonborg@gmail.com>
2017-01-25 23:18:41 -0500

    remove consants from geom models, change shapefile to reference constant correctly

--- app/models/concerns/geographic.rb
+++ app/models/concerns/geographic.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 module Geographic
+  FACTORY = RGeo::Geographic.simple_mercator_factory
+  EWKB = RGeo::WKRep::WKBGenerator.new(type_format:    :ewkb,
+                                       emit_ewkb_srid: true,
+                                       hex_format:     true)
+
   module ClassMethods
-
-    FACTORY = RGeo::Geographic.simple_mercator_factory
-    EWKB = RGeo::WKRep::WKBGenerator.new(type_format:    :ewkb,
-                                         emit_ewkb_srid: true,
-                                         hex_format:     true)
-
     def containing_latlon(lat, lon)
       ewkb = EWKB.generate(FACTORY.point(lon, lat).projection)
       where("ST_Intersects(geom, ST_GeomFromEWKB(E'\\\\x#{ewkb}'))")
--- lib/shapefiles.rb
+++ lib/shapefiles.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment.rb'
 
 def import_shapefile(dir:, shp_file:, model:, model_attr:, record_attr:)
   RGeo::Shapefile::Reader.
-    open(Rails.root.join('lib', 'shapefiles', dir, shp_file).to_s, factory: model::FACTORY) do |file|
+    open(Rails.root.join('lib', 'shapefiles', dir, shp_file).to_s, factory: Geographic::FACTORY) do |file|
     puts "File contains #{file.num_records} records."
     file.each do |record|
       puts "Record number #{record.index}:"
@@ -15,6 +15,9 @@ def import_shapefile(dir:, shp_file:, model:, model_attr:, record_attr:)
     end
   end
 end
+
+StateGeom.destroy_all
+DistrictGeom.destroy_all
 
 import_shapefile(dir:           'us_states_122116',
                  shp_file:      'cb_2015_us_state_500k.shp',
