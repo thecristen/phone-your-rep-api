fe1fb12b2de34f8ecb0f91ff7b88eadbbe5faf50
M. Simon Borg <msimonborg@gmail.com>
2017-01-25 20:56:55 -0500

    DRY up reps_controllers

--- app/controllers/api/v1/reps_controller.rb
+++ app/controllers/api/v1/reps_controller.rb
@@ -1,28 +1,6 @@
 # frozen_string_literal: true
 module Api
   module V1
-    class RepsController < ApplicationController
-      respond_to :json
-
-      def index
-        address = params[:address]
-        lat     = params[:lat]
-        long    = params[:long]
-        # return the first result, or a random one
-        @reps = if params
-                  Rep.find_em address: address, lat: lat, long: long
-                else
-                  # Would like to find requesting IP address, geocode it and return the closest rep
-                  # request = Rack::Request.new Rails.env
-                  # result = request.location
-                  # @office = OfficeLocation.near(result.postal_code)
-                  # @reps = @office.rep
-                  []
-                end
-        # respond_with @reps
-
-        render inline: MultiJson.dump(@reps), content_type: 'application/json'
-      end
-    end
+    class RepsController < Api::RepsController; end
   end
 end
