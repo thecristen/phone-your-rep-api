808d2216c32d666ad336c8c957795481393af325
M. Simon Borg <msimonborg@gmail.com>
2017-01-25 23:02:08 -0500

    add constants back to geographic module

--- app/models/concerns/geographic.rb
+++ app/models/concerns/geographic.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 module Geographic
   module ClassMethods
+
+    FACTORY = RGeo::Geographic.simple_mercator_factory
+    EWKB = RGeo::WKRep::WKBGenerator.new(type_format:    :ewkb,
+                                         emit_ewkb_srid: true,
+                                         hex_format:     true)
+
     def containing_latlon(lat, lon)
       ewkb = EWKB.generate(FACTORY.point(lon, lat).projection)
       where("ST_Intersects(geom, ST_GeomFromEWKB(E'\\\\x#{ewkb}'))")
