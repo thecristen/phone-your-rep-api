bdd779dc1ab1da92916dfbeae10c18c385b21b51
M. Simon Borg <msimonborg@gmail.com>
2017-01-26 13:19:12 -0500

    refactor issues controllers, paths, and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   # allows access to the API via http://herokuapp.com/api/any_endpoint
   # namespace :api, defaults: { format: :json }, path: '/api' do
   namespace :api, defaults: { format: 'json' } do
-    namespace :v1 do # versioning
+    namespace :v1 do # version 1
       resources :reps, only: [:index]
+      resources :issues, only: [:index, :new, :create, :update]
     end
+
     namespace :beta do # beta version
       resources :reps,   only: [:index]
       resources :issues, only: [:index, :new, :create, :update]
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
   get '/reps', to: 'reps#index'
   get '/reps/:bioguide_id', to: 'reps#show'
   # resources :reps, only: [:index, :show]
-  # resources :issues, only: [:index, :new, :create, :update]
+  resources :issues, only: [:index, :new, :create, :update]
 
   get '/v_cards/:id', to: 'v_cards#show'
 
