19ec262d90eee4ae514dc198fe2c00ed540935e3
M. Simon Borg <msimonborg@gmail.com>
2017-01-25 22:50:38 -0500

    add geom constants directly to models

--- app/models/district_geom.rb
+++ app/models/district_geom.rb
@@ -3,5 +3,10 @@ class DistrictGeom < ApplicationRecord
   extend Geographic::ClassMethods
   include Geographic::InstanceMethods
 
+  FACTORY = RGeo::Geographic.simple_mercator_factory
+  EWKB = RGeo::WKRep::WKBGenerator.new(type_format:    :ewkb,
+                                       emit_ewkb_srid: true,
+                                       hex_format:     true)
+
   belongs_to :district, foreign_key: :full_code, primary_key: :full_code
 end
--- app/models/state_geom.rb
+++ app/models/state_geom.rb
@@ -3,5 +3,10 @@ class StateGeom < ApplicationRecord
   extend Geographic::ClassMethods
   include Geographic::InstanceMethods
 
+  FACTORY = RGeo::Geographic.simple_mercator_factory
+  EWKB = RGeo::WKRep::WKBGenerator.new(type_format:    :ewkb,
+                                       emit_ewkb_srid: true,
+                                       hex_format:     true)
+
   belongs_to :state, foreign_key: :state_code, primary_key: :state_code
 end
